24138f7933a3be67aaef02fcd859e47788683990
nurulamin092 <mnurulamin092@gmial.com>
2022-03-22 07:41:22 +0600

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg';
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/hone">Home</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
