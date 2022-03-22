e13134ffaee9059b56b0ebec6aeaed23c2843c23
nurulamin092 <mnurulamin092@gmial.com>
2022-03-22 08:00:04 +0600

    shop grid add

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css'
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+
+                <h3>This is Products</h3>
+            </div>
+            <div className="cart-container">
+                <h4>This is order summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
