dc21bdc59e0a1903d483537f333afb292386043e
nurulamin092 <mnurulamin092@gmial.com>
2022-03-22 08:11:44 +0600

    product length show in display

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+
+    }, [])
     return (
         <div className='shop-container'>
             <div className="products-container">
 
-                <h3>This is Products</h3>
+                <h3>This is Products:{products.length}</h3>
             </div>
             <div className="cart-container">
                 <h4>This is order summary</h4>
