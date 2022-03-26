850c55faf5ea0351ec371ac7e1ca129cee080cc1
nurulamin092 <mnurulamin092@gmial.com>
2022-03-26 19:45:41 +0600

    cart code  update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,9 +3,12 @@ import './Cart.css'
 const Cart = ({ cart }) => {
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart) {
         total = total + product.price;
         shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
+
     }
     const tax = parseFloat((total * 0.1).toFixed(2));
     const grandTotal = total + shipping + tax;
